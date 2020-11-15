68a695cbb69fa9940c6623556a98ca5aed326fd8
Carlos Kayser <carloshkayser@gmail.com>
2020-11-15 13:45:10 -0300

    Python Bolt

---
+++ TwitterWordCount/src/main/java/io/github/carloshkayser/twitterwordcount/bolts/TweetReportPythonBolt.java
@@ -0,0 +1,25 @@
+package io.github.carloshkayser.twitterwordcount.bolts;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.ShellBolt;
+import org.apache.storm.topology.IRichBolt;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+
+import java.util.Map;
+
+public class TweetReportPythonBolt extends ShellBolt implements IRichBolt {
+
+    private OutputCollector collector;
+
+    public TweetReportPythonBolt() {
+        super("python3", "TweetUpdateDashboard.py");
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        // there is no output
+    }
+
+    @Override
+    public Map<String, Object> getComponentConfiguration() { return null; }
+}
